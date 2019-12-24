7fccf28a71e2a4d4d2960ba65fd88e9eda23c857
pinchez <pinchestheone@gmail.com>
2019-12-24 16:43:08 +0300

    already rejected  promise

--- promise-api.js
+++ promise-api.js
@@ -1,2 +1,7 @@
 // already resolved promise
-const p = Promise.resolve({id: 1}) .then(result => console.log(result))
+const Resolved = Promise.resolve({ id: 1 }).then(result => console.log(result))
+
+//Already rejected promise
+const Rejected = Promise.reject(
+  new Error('Reason for rejection...'),
+).catch(err => console.log(err.message))
