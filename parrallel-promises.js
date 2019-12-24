a528cc80c64cad2b7251c1d7157048c96ebffd04
pinchez <pinchestheone@gmail.com>
2019-12-24 17:04:54 +0300

    Promises.race

--- parrallel-promises.js
+++ parrallel-promises.js
@@ -1,8 +1,8 @@
 const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log('fetching promise one')
-    // resolve(1)
-    reject(new Error('Something went wrong!'))
+    resolve(1)
+    // reject(new Error('Something went wrong!'))
   }, 1000)
 })
 
@@ -13,6 +13,6 @@ const p2 = new Promise((resolve, reject) => {
   }, 1000)
 })
 
-Promise.all([p1, p2])
+Promise.race([p1, p2])
   .then(result => console.log(result))
   .catch(err => console.log(Error(err)))
